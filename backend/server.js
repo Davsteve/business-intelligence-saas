f9b35787effa997b87dfc6e84cfd3cbf7bf1184a
Davsteve <david22dayan@gmail.com>
2026-04-05 23:04:35 +0530

    Added express-rate-limit dependency

--- backend/server.js
+++ backend/server.js
@@ -420,6 +420,6 @@ return res.json({
 // 🚀 Start Server
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
